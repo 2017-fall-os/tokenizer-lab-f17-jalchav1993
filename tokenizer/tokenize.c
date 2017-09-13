5acb3e38a35ff78994fafdfc9f1c10771cc69f9c
Aex <aex@Aexs-MacBook-Pro.local>
2017-09-13 11:25:01 -0600

    skeleton

---
+++ tokenizer/tokenize.c
@@ -0,0 +1,13 @@
+// for puts
+#include <stdio.h>
+// for exit
+#include <stdlib.h>
+// for perror
+#include <errno.h>
+// for open()
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// for close
+#include <unistd.h>
+
